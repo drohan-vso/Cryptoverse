dd9f1c4076679c17cd2b5be516b04acadb656cdd
drohan-vso <87028255+drohan-vso@users.noreply.github.com>
2022-08-05 15:53:51 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react'
 // eslint-disable-next-line
-import { Switch,Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes,Route, Link } from 'react-router-dom';
 // eslint-disable-next-line
 import { Layout, Typography, Space } from 'antd';
-
-import { Navbar } from './components';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
 import './App.css'
-
-const App = () => {
+function App() {
   return (
     <div className='app'>
         <div className='navbar'>
             <Navbar />
         </div>
         <div className='main'>
-            
+          <div className='routes'>
+            <Routes>
+              <Route path="/" element={<Homepage />} /> 
+              <Route path="/exchanges" element={<Exchanges />} /> 
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} /> 
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} /> 
+              <Route path="/news" element={<News />} /> 
+            </Routes>  
+          </div>          
+          <div className='footer'>
+            <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+              Cryptoverse <br />
+              All rights reserved
+            </Typography.Title>
+           <Space>
+              <Link to="/">Home</Link>
+              <Link to="/exchanges">Exchanges</Link>
+              <Link to="/news">News</Link>
+            </Space>
         </div>
-        <div className='footer'>
-
         </div>
     </div>
-  )
+  );
 }
 
 export default App
